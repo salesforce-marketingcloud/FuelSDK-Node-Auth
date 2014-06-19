4ba409420cd7881dddf55a19b7711c42d12c8cd2
Kelly Andrews <kelly@kellyjandrews.com>
2014-06-19 15:29:59 -0400

    closing issue #1 - removed soap/rest specific requests

--- test/spec/fuelAuthTest.js
+++ test/spec/fuelAuthTest.js
@@ -14,56 +14,25 @@ var server;
 var client = new fuelAuth({
 	"authUrl" : "http://localhost:3000/auth",
 	"clientId" : '12345',
-	"clientSecret" : '12345',
-	"restEndpoint" : "http://localhost:3000",
-	"soapEndpoint" : "http://localhost:3000"
+	"clientSecret" : '12345'
 });
 
 
 describe('fuelAuth', function () {
-	before(function(){
-		server = app.listen(3000);
-	});
+	// before(function(){
+	// 	server = app.listen(3000);
+	// });
 
-	after(function(){
-		server.close();
-	});
+	// after(function(){
+	// 	server.close();
+	// });
 
 	it('should be an object', function(done) {
 		client.should.be.an.Object;
 		done();
 	});
 
-	it('should get a new token', function(done) {
-		console.log(client.getAccessToken());
-		//client.getAccessToken.should.be.an.Object;
-		done();
-	});
 
-	it('should refresh an expired token', function(done) {
-		client.should.be.an.Object;
-		done();
-	});
-
-	it('should make rest calls', function(done) {
-		client.should.be.an.Object;
-		done();
-	});
-
-	it('should update the token to make rest calls', function(done) {
-		client.should.be.an.Object;
-		done();
-	});
-
-	it('should make soap calls', function(done) {
-		client.should.be.an.Object;
-		done();
-	});
-
-	it('should update the token to make soap calls', function(done) {
-		client.should.be.an.Object;
-		done();
-	});
 
 });
 
